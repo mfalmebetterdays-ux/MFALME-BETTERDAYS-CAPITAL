eeb92ae0db16a41a38d85b4170bcdf089505afb6
Mesh <meshmwangi828@gmail.com>
2025-12-18 14:27:12 +0300

    g

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,16 +1,17 @@
-
+# myapp/admin.py
 from django.contrib import admin
-from . models import MfalmeUsers,Payments,Cart,Products,Journals,AvailableTickets,EventsPayments
+from .models import MfalmeUsers, VerificationCode
 
+@admin.register(MfalmeUsers)
+class MfalmeUsersAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'soldier_id', 'email_verified', 'date_joined')
+    list_filter = ('email_verified', 'is_active', 'date_joined')
+    search_fields = ('email', 'username', 'phone')
+    ordering = ('-date_joined',)
 
-
-
-
-# Register your models here.
-admin.site.register(MfalmeUsers)
-admin.site.register(Products)
-admin.site.register(Payments)
-admin.site.register(Cart)
-admin.site.register(Journals)
-admin.site.register(AvailableTickets)
-admin.site.register(EventsPayments)
+@admin.register(VerificationCode)
+class VerificationCodeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'code', 'is_used', 'is_expired', 'created_at')
+    list_filter = ('is_used', 'created_at')
+    search_fields = ('user__email', 'code')
+    ordering = ('-created_at',)
