f5290cda99b9b1950c87bfc1b0c0d87d94f19155
mfalmebetterdays-ux <mfalmebetterdays@gmail.com>
2026-02-22 17:34:48 +0300

    n

--- dict/settings.py
+++ dict/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 # ===== STATIC FILES CONFIGURATION =====
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
